d2bc2bbc170e27a2fb1d74e009be26b7d6eaab66
daniel <dani30316diez@gmail.com>
2022-10-06 12:42:55 +0200

    registro

--- app/src/main/java/com/example/reto/loginActivity.kt
+++ app/src/main/java/com/example/reto/loginActivity.kt
@@ -1,14 +1,23 @@
 package com.example.reto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.reto.databinding.ActivityLoginBinding
+import com.example.reto.databinding.ActivityRegisterBinding
 
 class loginActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        //abrir ventana de registro
+        binding.buttonregister.setOnClickListener {
+            startActivity(Intent(this, registerActivity::class.java))
+        }
 
     }
 }
--- app/src/main/java/com/example/reto/registerActivity.kt
+++ app/src/main/java/com/example/reto/registerActivity.kt
@@ -2,14 +2,17 @@ package com.example.reto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AlertDialog
 import com.example.reto.databinding.ActivityRegisterBinding
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.*
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.Query
 
 
 class registerActivity : AppCompatActivity() {
+
     var numuser = 0
     private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +20,14 @@ class registerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.buttonRegistrar.setOnClickListener(){
+                userregister()
+        }
 
 
     }
     fun userregister(){
+        val terminos = binding.terminos.isChecked
         //registrar usuario nuevo
         if(binding.textNombre.text.isNotEmpty() && binding.textNombre.text.isNotEmpty() &&
             binding.textApellidos.text.isNotEmpty() && binding.textCorreoRegis.text.isNotEmpty()
@@ -29,49 +35,76 @@ class registerActivity : AppCompatActivity() {
                 binding.textPassRegConfirmar.text.isNotEmpty()){
 
             var idusuario : Int = generariduser()
+            idusuario++
+            println(idusuario)
             val nombre = binding.textNombre.text.toString()
             val apellidos = binding.textApellidos.text.toString()
             val email = binding.textCorreoRegis.text.toString()
             val localidad = binding.textLocalidad.text.toString()
             val pass = binding.textPasswordReg.text.toString()
-            val confpass = binding.textPassRegConfirmar.toString()
+            val confpass = binding.textPassRegConfirmar.text.toString()
             val reportes = 0
             var saldo = 0
+
+            println("${pass} + ${confpass}" )
+
             val db = FirebaseFirestore.getInstance()
             // comprobar contraseñas iguales
-            if(pass.equals(confpass)) {
+
+
+            if(pass == confpass) {
+                if(terminos == true){
                 //comprobar el numero de usuarios inferior a 100
-                if(numuser<=100){
-                    saldo = 1
-                }
-                val datos = hashMapOf(
-                    "id" to idusuario,
-                    "nombre" to nombre,
-                    "apellidos" to apellidos,
-                    "email" to email,
-                    "localidad" to localidad,
-                    "reportes" to reportes,
-                    "saldo" to saldo
-                )
-                FirebaseAuth.//terminar
-                db.collection("Usuarios")
-                    .document()
-                    .set(datos)
-                    .addOnSuccessListener { resultado ->
-                    //mensaje exito
+                    if(idusuario<=100){
+                        saldo = 1
                     }
-                    .addOnFailureListener{ Exception ->
-                    //mensaje de error
+                        val datos = hashMapOf(
+                        "id" to idusuario,
+                        "nombre" to nombre,
+                        "apellidos" to apellidos,
+                        "email" to email,
+                        "localidad" to localidad,
+                        "reportes" to reportes,
+                        "saldo" to saldo,
+                        "pass" to pass
+                        )
+                        println(datos)
+                        FirebaseAuth.getInstance()
+                        .createUserWithEmailAndPassword(binding.textCorreoRegis.text.toString(),binding.textPasswordReg.text.toString())
+                        .addOnCompleteListener(){
+                                if(it.isSuccessful){
+                                    showpalertregistrobien()
+
+                                        db.collection("Usuarios")
+                                        .document(idusuario.toString())
+                                        .set(datos)
+                                        .addOnSuccessListener { resultado ->
+                                        //mensaje exito
+                                        showpalertregistrobien()
+                                        }
+                                        .addOnFailureListener{ Exception ->
+                                        println("error bbdd")
+                                        showpalertregistroerror()
+                                        }
+
+
+                                    }
+                                else{
+                                println("error autenticacion")
+                                showpalertregistroerror()
+                                }
+
+                            }
+
+                        }else{
+                    showpalertterminos()
+
                     }
 
-
-
-
-            }
-                else  {
+                    }else  {
                     //mensaje de error por contraseña no coincide
-                }
-
+                    showpalerterrorpass()
+                            }
             }
             else {
                 //mensaje de campos vacios
@@ -83,17 +116,29 @@ class registerActivity : AppCompatActivity() {
 
     }
     private fun generariduser(): Int {
+        var cod :String=""
         val db = FirebaseFirestore.getInstance()
-        db.collection("cantidadusuarios")
+        db.collection("Usuarios")
+                //ordenar por desceniente limite de 1 haciendo q solo coja el mas grande
+            .orderBy("id", Query.Direction.DESCENDING).limit(1)
             .get()
             .addOnSuccessListener { docs->
-                var datos = ""
+                var dato = ""
                 for  (documento in docs){
 
-                    datos += "${documento.data}\n"
+                    dato += "${documento.get("id")}\n"
+
+
+                     cod = dato.get(0).toString()
+
+                    println("preuba numero " + cod)
 
                 }
-                numuser = datos.toInt()
+                numuser = cod.toInt()
+            }
+            .addOnFailureListener(){
+                println("fallo")
+
             }
         return numuser
     }
@@ -106,7 +151,42 @@ class registerActivity : AppCompatActivity() {
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
+    private fun showpalertregistroerror(){
+        //alerta de error de registro
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Error al registrar, la contraseña deve inclur mayus y minus y numeros")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    private fun showpalertregistrobien(){
+        //alerta de exito de registro
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("registrado")
+        builder.setMessage("Registrado con exito")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
 
 
-
+    }
+    private fun showpalerterrorpass(){
+        //alerta de contraseña no coincide
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("error")
+        builder.setMessage("Error las contraseñas no cinciden")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    private fun showpalertterminos(){
+        //alerta de terminso y coniciones
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Por favor acepta lso terminos y condiciones de uso")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
 }
