51e2a1848600b23165af6e51823517f996750e05
daniel <dani30316diez@gmail.com>
2022-10-06 13:02:19 +0200

    login

--- app/src/main/java/com/example/reto/loginActivity.kt
+++ app/src/main/java/com/example/reto/loginActivity.kt
@@ -3,8 +3,10 @@ package com.example.reto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.reto.databinding.ActivityLoginBinding
 import com.example.reto.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class loginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -18,6 +20,65 @@ class loginActivity : AppCompatActivity() {
         binding.buttonregister.setOnClickListener {
             startActivity(Intent(this, registerActivity::class.java))
         }
+        binding.buttonlogin.setOnClickListener{
+            login()
 
+
+        }
+    }
+
+    fun login(){
+        //funcion de login
+        if(binding.txtemail.text.isNotEmpty() && binding.txtpass.text.isNotEmpty()){
+        val email = binding.txtemail.text.toString()
+        val pass = binding.txtpass.text.toString()
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,pass).addOnCompleteListener {
+                if(it.isSuccessful){
+                    loginexitoso()
+
+                }
+                else {
+                    errorlogin()
+                }
+
+
+            }
+
+
+        }
+        else{
+            camposvacios()
+
+        }
+
+
+
+    }
+    private fun errorlogin(){
+        //alerta de usuario incorrecto
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Error Usuario o contraseña incorrectos")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    private fun loginexitoso(){
+        //alerta temporal
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("login exitoso")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    private fun camposvacios(){
+        //alerta de campos vacios
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("error rellene los campos")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
