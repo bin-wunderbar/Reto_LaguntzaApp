88f55fda81d5365e2c35817e7f8e0dde2f73195b
daniel <dani30316diez@gmail.com>
2022-10-10 13:55:34 +0200

    admin

--- app/src/main/java/com/example/reto/Admin_Activity.kt
+++ app/src/main/java/com/example/reto/Admin_Activity.kt
@@ -1,11 +1,36 @@
 package com.example.reto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.reto.databinding.ActivityAdminBinding
 
 class Admin_Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityAdminBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+        binding = ActivityAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //listar todos los usuarios
+        binding.btnradioListarUser.setOnClickListener(){
+            val intent = Intent (this, ListAllUser_Activity::class.java).apply {
+
+            }
+            startActivity(intent)
+        }
+        //listar solo usuarios con reportes
+        binding.btnradioReportUser.setOnClickListener(){
+            //val intent = Intent(this, ReportUserActivity::class.java).apply {
+            val intent = Intent (this, ListAllUser_Activity::class.java).apply {
+
+            }
+            startActivity(intent)
+        }
+
+
+
+
     }
 }
