23bc8cb7a1463dedff902947cf8e3eacfa2fb940
Youssef <binwunderbar@gmail.com>
2022-10-17 14:06:07 +0200

    arreglo de publicidad

--- app/src/main/java/com/example/reto/Globales.kt
+++ app/src/main/java/com/example/reto/Globales.kt
@@ -6,15 +6,21 @@ import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import com.example.reto.Publicidad
 
-fun activarPublicidad( actividad: AppCompatActivity) {
+fun activarPublicidad(actividad: AppCompatActivity) {
+
     actividad.lifecycleScope.launch {
         withContext(Dispatchers.IO) {
-            // tiempo en el que espera para mostrar publicidad cada vez
-            for (i in 90 downTo 1) {
-                Thread.sleep(1000)
+            // comprueba si la actividad esta a la vista se inicia el contador
+            if (!actividad.isFinishing){
+                // tiempo en el que espera para mostrar publicidad cada vez
+                for (i in 60 downTo 1) {
+                    Thread.sleep(1000)
+                }
             }
         }
         actividad.startActivity(Intent(actividad, Publicidad::class.java))
+
     }
 }
